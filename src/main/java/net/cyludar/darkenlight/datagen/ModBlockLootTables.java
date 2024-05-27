b37f29577e342ca1b43ad49441df53c82709eac9
BoiledEggs <145751719+cutEggs@users.noreply.github.com>
2024-05-28 00:56:13 +0800

    My ores are succeeded in generating underground, and it took me 2 weeks for a stupid reason like I misnamed the file.

---
+++ src/main/java/net/cyludar/darkenlight/datagen/ModBlockLootTables.java
@@ -0,0 +1,32 @@
+package net.cyludar.darkenlight.datagen;
+
+
+import net.cyludar.darkenlight.block.ModBlocks;
+import net.cyludar.darkenlight.item.ModItems;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+    public ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.JADE_BLOCK.get());
+
+        add(ModBlocks.JADE_ORE.get(),
+                (block) -> createOreDrop(ModBlocks.JADE_ORE.get(), ModItems.JADE.get()));
+        add(ModBlocks.DEEPSLATE_JADE_ORE.get(),
+                (block) -> createOreDrop(ModBlocks.DEEPSLATE_JADE_ORE.get(), ModItems.JADE.get()));
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
+}
--- src/main/java/net/cyludar/darkenlight/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/cyludar/darkenlight/worldgen/ModPlacedFeatures.java
@@ -1,10 +1,13 @@
 package net.cyludar.darkenlight.worldgen;
 
+
 import net.cyludar.darkenlight.darkenlight;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -16,19 +19,20 @@ import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import java.util.List;
 
 public class ModPlacedFeatures {
-    public static final ResourceKey<PlacedFeature> JADE_ORE_PLACED_KEY = createKey("jade_placed");
+    public static final ResourceKey<PlacedFeature> JADE_PLACED_KEY = createKey("jade_placed");
 
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
-        register(context, JADE_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_JADE_ORE_KEY),
+        register(context, JADE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_JADE_ORE_KEY),
                 ModOrePlacement.commonOrePlacement(3, // veins per chunk
-                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-80), VerticalAnchor.absolute(80))));
+                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.absolute(80))));
+
     }
 
-    private static ResourceKey<PlacedFeature> createKey(String name) {
-        return ResourceKey.create((Registries.PLACED_FEATURE), new ResourceLocation(darkenlight.MOD_ID, name));
 
+    private static ResourceKey<PlacedFeature> createKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(darkenlight.MOD_ID, name));
     }
 
     private static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration,
@@ -40,6 +44,4 @@ public class ModPlacedFeatures {
                                  PlacementModifier... modifiers) {
         register(context, key, configuration, List.of(modifiers));
     }
-
-
 }
